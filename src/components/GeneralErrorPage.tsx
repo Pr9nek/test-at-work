f03e630f2af1426c6dda5e78c2c1c2dc58bbf645
Alex <pr9nek@mail.ru>
2025-10-23 17:07:09 +0400

    refact: more responcible form with user friendly messages

--- src/components/GeneralErrorPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function GeneralErrorPage() {
-    return (
-        <div style={{ padding: 20, color: 'red' }}>
-            <h2>Произошла ошибка загрузки!</h2>
-            <p>Не удалось получить данные для этой страницы. Пожалуйста, проверьте подключение к сети.</p>
-        </div>
-    );
-}
